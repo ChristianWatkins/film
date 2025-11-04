0fcf24244502d686b7171227282ae973fc8b98d2
ChristianWatkins <christian.watkins@gmail.com>
2025-11-04 09:41:04 +0100

    improve X icon look to be like others

--- components/FilmCard.tsx
+++ components/FilmCard.tsx
@@ -356,7 +356,9 @@ export default function FilmCard({ film, isFlipped, onFlip, onGenreClick, onWatc
               onClick={handleRemoveClick}
               title="Remove from list"
             >
-              <XMarkIcon className="w-5 h-5 text-white" />
+              <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
             </div>
           )}
           {!showRemoveButton && showWatchedToggle && (
@@ -564,7 +566,9 @@ export default function FilmCard({ film, isFlipped, onFlip, onGenreClick, onWatc
               onClick={handleRemoveClick}
               title="Remove from list"
             >
-              <XMarkIcon className="w-5 h-5 text-white" />
+              <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
             </div>
           )}
           {!showRemoveButton && showWatchedToggle && (
